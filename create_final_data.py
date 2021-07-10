7572e6bc49ade5ade6c114d5d27d60dc2b2c21d0
ViktoriaBrand <62878599+ViktoriaBrand@users.noreply.github.com>
2021-07-10 13:24:57 +0200

    Normalization of GSR added

--- create_final_data.py
+++ create_final_data.py
@@ -13,19 +13,31 @@ import pandas as pd
 df_gsr = pd.read_excel('COINs Intelektuellen-Ranking.xlsx')
 df_gsr.rename(columns={'Google Search Results (this year)': 'GSR'}, inplace=True)
 
+#Normalization of Google Search Results with min-max normalization
+df_gsr['GSR_score']=(df_gsr['GSR']-df_gsr['GSR'].min())/(df_gsr['GSR'].max()-df_gsr['GSR'].min())
+
+
 #Get Wikipedia Score
 df_wikipedia = pd.read_csv('Thoughtleader_Wikipedia.csv')
+
 
 #Get Sentiment Score of Google News
 df_sentiment = pd.read_csv('Sentiment_Index.csv', sep=';')
 df_sentiment.rename(columns={'Index': 'Sentiment_score'}, inplace=True)
 
+
 #Get Twitter Score
 #df_twitter = pd.read_csv('')
 
+
 #Merge all dataframes
-df_thoughtleaders = pd.merge(df_gsr[['Name', 'GSR']], df_wikipedia[['Name', 'Wikipedia_score']], on='Name', how='left')
+df_thoughtleaders = pd.merge(df_gsr[['Name', 'GSR_score']], df_wikipedia[['Name', 'Wikipedia_score']], on='Name', how='left')
 df_thoughtleaders = pd.merge(df_thoughtleaders, df_sentiment, on='Name', how='left')
 #df_thoughtleaders = pd.merge(df_thoughtleaders, df_twitter, on='Name', how='left')
 
-df_thoughtleaders.to_csv('Thoughtleaders_final.csv')
+
+#Calculate overall Thoughtleader Score
+#df_thoughtleaders['Thoughtleader_Score']=(df_thoughtleaders['GSR_score']+df_thoughtleaders['Wikipedia_score']+df_thoughtleaders['Sentiment_score']+df_thoughtleaders['Twitter_score'])/4
+
+print(df_thoughtleaders)
+df_thoughtleaders.to_csv('Thoughtleaders_final.csv', index=False)
