465a126302304a5a5cd04e03aeda531e5cf65750
bertej2 <64323715+bertej2@users.noreply.github.com>
2021-07-16 23:55:06 +0200

    last google news change before bringing to main

--- googleNews_fetching.py
+++ googleNews_fetching.py
@@ -11,17 +11,14 @@ Also max of results in one run are 10 articles -> hence the procedure:
     One run is for half a month, after that combined the data rows in Excel manually
     - time consuming but better than desperately searching for a work around that might 
     not work in the end anyway!
-    
+Prodecure: 
+    Searching for the author and fetching data twice a month, from beginning to middle, 
+    from middle to end of month in 2021. Last month: 01.05. to 10.05. 
 @author: Viktoria
 Added the try except as downloading of articles sometime did not work 
 which resulted in not getting any articles for the specific time period
 """
 
-# authors = ['Lukas Bärfuss', 'Jonas Lüscher', 'Charles Lewinski', 'Adolf Muschg', 
-#            'Ludwig Hasler', 'Martin Meyer', 'Ueli Mäder', 'Martin R. Dean', 
-#            'Peter Bichsel', 'Jürg Halter', 'Rolf Dobelli', 'Thomas Hürlimann','Milena Moser', 
-#            'Barbara Bleisch', 'Annemarie Piper', 'Sibylle Berg', 'Katja Rost', 
-#            'Zora del Buono', 'Katja Gentinetta', 'Miriam Meckel']
    
 from GoogleNews import GoogleNews
 import pandas as pd
@@ -53,29 +50,7 @@ for ind in df.index:
 df['summary'] = df.index.map(dict)
 df.to_excel("articles_Buono_Mar_1.xlsx")
 
-"""
-Created on Tue May 11 15:50:02 2021
 
-@author: Johanna
-The following part is for coverting any XLSX to JSON  
-keep in mind that this version directly coverts the data to a json file
-the file has to be renamed in the folder accordingly afterwards
-"""
-# excelFilePath = 'C:/Users/Johanna/.spyder-py3/Authors/Lukas Bärfuss/Bärfuss_all_2021.xlsx'
-# import excel2json
-# excel2json.convert_from_file(excelFilePath)
-
-"""
-TO DO
-- check data for correctness: 
-    - check if the articles are actually for the authors cause I noticed that some aren't for the authors e.g. Hasler and Meyer 
-    - some empty values for summary since no data available apparently
-    - check for duplicates on title and remove 
-    - remove unimportant columns like img and datatime 
-    - when the above steps are done, continue converting the excel to json 
-    - find out further steps for an efficient analysis in Swiss Tribeleaders and their influence 
-    - maybe check articles for other mentioned personas that could be candidates for the tribe 
-"""
 
 
 
