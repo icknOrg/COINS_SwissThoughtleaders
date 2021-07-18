b403d850e339f4c2ee1113175b0d719911532294
ViktoriaBrand <62878599+ViktoriaBrand@users.noreply.github.com>
2021-07-18 23:01:54 +0200

    added to comments to the min-max normalization of twitter and wikipedia

--- Twitter_final.py
+++ Twitter_final.py
@@ -193,7 +193,10 @@ def get_twitter_factor_SW():
     twitter_factor_SW = pd.DataFrame(twitter_SW)
     twitter_factor_SW.replace([np.inf, -np.inf], np.nan, inplace=True)
     twitter_factor_SW = twitter_factor_SW.fillna(0)
+    
+    #Normalize with min_max -> When executing the thoughtleader_index_all.py and the create_classification_all_df.py files then this needs to be commented out
     #twitter_factor_SW['Twitter']=(twitter_factor_SW['Twitter']-twitter_factor_SW['Twitter'].min())/(twitter_factor_SW['Twitter'].max()-twitter_factor_SW['Twitter'].min())
+    
     return twitter_factor_SW;
 
 def get_twitter_factor_DE(): 
@@ -202,7 +205,10 @@ def get_twitter_factor_DE():
    twitter_factor_DE = pd.DataFrame(twitter_DE)
    twitter_factor_DE.replace([np.inf, -np.inf], np.nan, inplace=True)
    twitter_factor_DE = twitter_factor_DE.fillna(0)
+   
+   #Normalize with min_max -> When executing the thoughtleader_index_all.py and the create_classification_all_df.py files then this needs to be commented out
    #twitter_factor_DE['Twitter']=(twitter_factor_DE['Twitter']-twitter_factor_DE['Twitter'].min())/(twitter_factor_DE['Twitter'].max()-twitter_factor_DE['Twitter'].min())
+   
    return twitter_factor_DE;
 
 
