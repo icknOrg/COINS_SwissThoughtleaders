88c8dff0abedfb094a47b705d10711de21c67006
ViktoriaBrand <62878599+ViktoriaBrand@users.noreply.github.com>
2021-07-18 17:28:18 +0200

    Added the Calculation of the Thoughtleader score for all people

---
+++ create_classification_all_df.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 21:54:22 2021
+
+@author: vikto
+
+load the different dfs and tables with the data that we want to use for the classification
+and calculate the classification labels based on the Thoughtleader scores of the dataframe 
+with both the German and the Swiss people  
+"""
+
+import pandas as pd
+import numpy as np
+
+from Twitter_final import get_twitter_original_SW
+from Twitter_final import get_twitter_original_DE
+from Wikipedia import get_wikipedia_SW
+from Wikipedia import get_wikipedia_DE
+from thoughtleader_index_all import get_thoughtleader_score
+
+
+tl_SW = pd.read_excel(r'CSV Data/COINs Intelektuellen-Ranking.xlsx')
+tl_DE = pd.read_excel(r'CSV Data/Thoughtleader List_GermanSpeaking.xlsx')
+twitter_DE = get_twitter_original_DE()
+twitter_SW = get_twitter_original_SW()
+wikipedia_DE = get_wikipedia_DE()
+wikipedia_SW = get_wikipedia_SW()
+score = get_thoughtleader_score()
+
+
+def prepare_data(data, wikipedia, twitter, score):
+    
+    data.rename(columns={'Google Search Results (this year)': 'GSR'}, inplace=True)
+    data.Twitter = data.Twitter.str.replace("@", "")
+    data = data.rename(columns={'Twitter' : 'id'})
+    
+    # get original twitter data and add the verified part just in case for now 
+    twitter_data = twitter
+    twitter_data = twitter_data.drop(columns=['name'])
+    
+    conditions = [
+        (data['Twitter Verifiziert?'] == 'nicht verifiziert') | (data['Twitter Verifiziert?'] == 0),
+        (data['Twitter Verifiziert?'] == 'ja')]
+    
+    values = [0, 1]
+    
+    data['verified'] = np.select(conditions, values)
+    
+    # Get Sentiment Score of Google News
+    #df_sentiment = pd.read_csv('Sentiment_Index.csv', sep=';')
+    #df_sentiment.rename(columns={'Index': 'Sentiment_score'}, inplace=True)
+    
+    # get wikipedia
+    wikipedia_data = wikipedia
+    
+    # get thoughtleader score
+    thoughtleader_score = score
+    
+    # Merge all dataframes
+    class_data = pd.merge(data, twitter_data, on='id', how='left')
+    class_data = pd.merge(class_data, wikipedia_data, on='Name', how='left')
+    class_data = pd.merge(class_data, thoughtleader_score, on='Name', how='left')
+    #class_data = pd.merge(class_data, df_sentiment[['Name', 'Sentiment_score']], on='Name', how='left')
+    class_data.fillna(0, inplace=True)
+    class_data.drop_duplicates(subset=['Name'], inplace=True)
+
+    # get percentage, top 30%
+    x = class_data[['Thoughtleader_Score']].sort_values(by='Thoughtleader_Score',ascending=False)[(class_data[['Thoughtleader_Score']].sort_values(by='Thoughtleader_Score',ascending=False).cumsum()
+                                                     /class_data[['Thoughtleader_Score']].sort_values(by='Thoughtleader_Score',ascending=False).sum())<=.3].dropna()
+    
+    # add labels
+    class_labels = []
+    for value in class_data.index: 
+        if value in x.index:
+            class_labels.append(1)
+        else:
+            class_labels.append(0)
+    
+    class_data['class_labels'] = class_labels
+    return class_data
+
+Classification_DE = prepare_data(tl_DE, wikipedia_DE, twitter_DE, score);
+Classification_SW = prepare_data(tl_SW,  wikipedia_SW, twitter_SW, score);
+
+# function to retrieve classification data in the code for the classification model
+def get_class_original_DE():
+    global classification_data_de; 
+    classification_data_de = pd.DataFrame(Classification_DE)
+    classification_data_de.to_csv(r'CSV Data/Classified_Thoughtleaders_DE.csv')
+    return classification_data_de;
+
+def get_class_original_SW():
+    global classification_data_sw; 
+    classification_data_sw = pd.DataFrame(Classification_SW)
+    classification_data_sw.to_csv(r'CSV Data/Classified_Thoughtleaders_SW.csv')
+    return classification_data_sw;
+
---
+++ thoughtleader_index_all.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 10 12:18:35 2021
+
+@author: vikto
+
+create final dataframe with Thoughtleader score for both the German and the Swiss people
+"""
+
+import pandas as pd
+from Twitter_final import get_twitter_factor_SW
+from Twitter_final import get_twitter_factor_DE
+from Wikipedia import get_wikipedia_score_SW
+from Wikipedia import get_wikipedia_score_DE
+
+#get data of .xlsx, .csv files and from other scripts
+tl_SW = pd.read_excel(r'CSV Data/COINs Intelektuellen-Ranking.xlsx')
+tl_DE = pd.read_excel(r'CSV Data/Thoughtleader List_GermanSpeaking.xlsx')
+twitter_score_DE = get_twitter_factor_DE()
+twitter_score_SW = get_twitter_factor_SW()
+wikipedia_score_DE = get_wikipedia_score_DE()
+wikipedia_score_DE = wikipedia_score_DE.fillna(0)
+wikipedia_score_SW = get_wikipedia_score_SW()
+wikipedia_score_SW = wikipedia_score_SW.fillna(0)
+sentiment_DE = pd.read_csv(r'CSV Data/Sentiment_Index_DE.csv', sep=';')
+sentiment_SW = pd.read_csv(r'CSV Data/Sentiment_Index.csv',  sep=';')
+
+tl = pd.concat([tl_SW, tl_DE])
+twitter = pd.concat([twitter_score_SW, twitter_score_DE])
+wikipedia_score = pd.concat([wikipedia_score_DE, wikipedia_score_SW])
+sentiment = pd.concat([sentiment_DE, sentiment_SW])
+
+#Normalization of twitter and wikipedia
+wikipedia_score['Wikipedia_score']=(wikipedia_score['Wikipedia_score']-wikipedia_score['Wikipedia_score'].min())/(wikipedia_score['Wikipedia_score'].max()-wikipedia_score['Wikipedia_score'].min())
+twitter['Twitter']=(twitter['Twitter']-twitter['Twitter'].min())/(twitter['Twitter'].max()-twitter['Twitter'].min())
+
+def prepare_data(df_gsr, wikipedia_score, twitter_score, sentiment_score):
+    
+    #get Google Search Result
+    df_gsr.rename(columns={'Google Search Results (this year)': 'GSR'}, inplace=True)
+    
+    #Normalization of Google Search Results with min-max normalization
+    df_gsr['GSR_score']=(df_gsr['GSR']-df_gsr['GSR'].min())/(df_gsr['GSR'].max()-df_gsr['GSR'].min())
+    
+    
+    #Get Wikipedia Score
+    df_wikipedia = wikipedia_score
+    
+    #Get Sentiment Score of Google News
+    df_sentiment = sentiment_score
+    df_sentiment.rename(columns={'Index': 'Sentiment_score'}, inplace=True)
+    
+    
+    #Get Twitter Score
+    df_twitter = twitter_score
+    #pd.read_csv('Thoughtleader_Twitter.csv')
+    df_twitter.rename(columns={'Twitter': 'Twitter_score'}, inplace=True)
+    
+    
+    #Merge all dataframes
+    df_thoughtleaders = pd.merge(df_gsr[['Name', 'GSR_score']], df_wikipedia[['Name', 'Wikipedia_score']], on='Name', how='left')
+    df_thoughtleaders = pd.merge(df_thoughtleaders, df_sentiment[['Name', 'Sentiment_score']], on='Name', how='left')
+    df_thoughtleaders = pd.merge(df_thoughtleaders, df_twitter[['Name', 'Twitter_score']], on='Name', how='left')
+    df_thoughtleaders.fillna(0, inplace=True)
+    
+    #Calculate overall Thoughtleader Score
+    df_thoughtleaders['Thoughtleader_Score']=(df_thoughtleaders['GSR_score']+df_thoughtleaders['Wikipedia_score']+df_thoughtleaders['Sentiment_score']+df_thoughtleaders['Twitter_score'])/4
+   
+    return df_thoughtleaders
+    
+
+
+def get_thoughtleader_score():
+    global df_thoughtleaders; 
+    Thoughtleaders = prepare_data(tl, wikipedia_score, twitter, sentiment);
+    df_thoughtleaders = pd.DataFrame(Thoughtleaders)
+    df_thoughtleaders.drop_duplicates(subset=['Name'], inplace=True)
+    df_thoughtleaders.to_csv(r'CSV Data/Thoughtleaders_final.csv', index=False)
+    return df_thoughtleaders;
+
+test = get_thoughtleader_score()
+
+
+
+
